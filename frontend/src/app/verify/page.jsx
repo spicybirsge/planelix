793a2347fd2679efda12b120ad4e3cf3779b2604
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-09-08 20:12:22 +0530

    implemented basics of verify endpoint

---
+++ frontend/src/app/verify/page.jsx
@@ -0,0 +1,28 @@
+
+export const metadata = {
+    title: 'Verify your planelix account',
+    description: '',
+    robots: "noindex",
+    keywords: [],
+    icons: {
+      icon: '/assets/logo.png',
+  
+    },
+    openGraph: {
+      title: '',
+      description: '',
+      site_name: '',
+      images: [
+        {
+          url: '',
+        },
+      ],
+      robots: 'noindex'
+    },
+  
+  };
+
+
+  export default function Verify() {
+    return <h1>Hello World</h1>
+  }
