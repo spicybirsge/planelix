c5c4c9968892398a6a5756a48caee197828cde43
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-10-05 20:16:52 +0530

    a

--- frontend/src/components/NewPostComponent.jsx
+++ frontend/src/components/NewPostComponent.jsx
@@ -5,32 +5,68 @@ import SideBar from './SideBar';
 import authenticate from '@/functions/authenticate';
 import Loading from './Loading';
 import { useRouter } from 'next/navigation';
+import { Input, InputGroup, Text, Center, Container, Card, CardBody, CardHeader, Button, Textarea, Divider, Select } from '@chakra-ui/react'
 export default function NewPostComponent() {
 
     const router = useRouter()
     const userState = user((state) => state.user);
     const loadedState = user((state) => state.loaded);
-    
-    
-    
+
+
+
     useEffect(() => {
 
-        if(!loadedState) {
+        if (!loadedState) {
             authenticate()
         }
 
 
     }, [])
 
-    if(!loadedState) {
+    if (!loadedState) {
         return <Loading></Loading>
     }
 
-    if(loadedState && !userState) {
+    if (loadedState && !userState) {
 
-        router.push("/login", {scroll: false})
+        router.push("/login", { scroll: false })
     }
+
+
+
+    const element = <Container ><Card>
+        <CardBody>
+
+            <Center><Text fontSize='3xl' as={"b"} style={{ marginBottom: '15px' }}>Create a new Post</Text></Center>
+
+
+            <Text as={"p"}>Aircraft Model</Text>
+            <InputGroup >
+                <Input type="text" placeholder={"Aircraft model name in your post attachment (optional)"} style={{ marginBottom: '10px' }}></Input>
+            </InputGroup>
+            <Text as={"p"}>Caption</Text>
+            <InputGroup >
+                <Textarea type="text" placeholder={"Your posts caption (optional)"} style={{ marginBottom: '10px' }}></Textarea>
+            </InputGroup>
+            <Text as={"p"}>Category</Text>
+            <InputGroup >
+                <Select >
+                    <option value='take-off'>take off</option>
+                    <option value='in-sky'>in sky</option>
+                    <option value='landing'>landing</option>
+                    <option value='other'>other</option>
+                </Select>
+            </InputGroup>
+
+            <Text as={"p"} >Post attachment</Text>
+            <InputGroup >
+
+                <input type="file" accept="image/jpeg, image/png, image/jpg, image/gif, video/mp4, video/webm, video/ogg" multiple></input>
+            </InputGroup>
+            <Button colorScheme={"orange"} style={{ marginTop: '10px' }}>Post</Button>
+        </CardBody>
+    </Card></Container>
     return <SideBar loggedIn={userState ? true : false} active={"new"} user={userState}
-    
-    element={<h1>dummy element</h1>}></SideBar>
+
+        element={element}></SideBar>
 }
