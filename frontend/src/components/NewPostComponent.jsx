81866d549370828f4d2ad3ec16fbee3a9cbbfb50
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-10-05 18:51:27 +0530

    added auth system to new post component

--- frontend/src/components/NewPostComponent.jsx
+++ frontend/src/components/NewPostComponent.jsx
@@ -1,9 +1,34 @@
 'use client'
-import {useState, useEffect} from 'react';
-import {user} from '@/state/store'
+import { useState, useEffect } from 'react';
+import { user } from '@/state/store'
 import SideBar from './SideBar';
-
+import authenticate from '@/functions/authenticate';
+import Loading from './Loading';
+import { useRouter } from 'next/navigation';
 export default function NewPostComponent() {
 
-    return <SideBar loggedIn={true} active={"new"} user={{username: 'hello', name: 'j'}}></SideBar>
+    const router = useRouter()
+    const userState = user((state) => state.user);
+    const loadedState = user((state) => state.loaded);
+    
+    
+    
+    useEffect(() => {
+
+        if(!loadedState) {
+            authenticate()
+        }
+
+
+    }, [])
+
+    if(!loadedState) {
+        return <Loading></Loading>
+    }
+
+    if(loadedState && !userState) {
+
+        router.push("/login", {scroll: false})
+    }
+    return <SideBar loggedIn={userState ? true : false} active={"new"} user={userState}></SideBar>
 }
